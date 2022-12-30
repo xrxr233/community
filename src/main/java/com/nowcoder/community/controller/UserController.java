4e7d68d499a744a78f381c71053f4d76ba5ba537
xuren <xuren18151310314@163.com>
2022-12-30 22:32:00 +0800

    关注、取消关注

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowService;
 import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -26,7 +28,7 @@ import java.io.IOException;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements CommunityConstant {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Value("${community.path.upload}")
@@ -43,6 +45,9 @@ public class UserController {
 
     @Autowired
     private LikeService likeService;
+
+    @Autowired
+    private FollowService followService;
 
     @Autowired
     private HostHolder hostHolder;
@@ -163,6 +168,21 @@ public class UserController {
         //用户信息
         model.addAttribute("user", user);
         model.addAttribute("likeCount", likeService.findUserLikeCount(userId));  //获赞数
+
+        //关注数量（关注的用户数量）
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+
+        //粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+
+        //当前用户是否关注该用户
+        boolean hasFollowed = false;
+        if(hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
 
         return "/site/profile";
     }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -7,6 +7,8 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
 
     /* 获取某个实体（帖子、评论）的赞的key
     *  key: like:entity:entityType:entityId
@@ -22,5 +24,33 @@ public class RedisKeyUtil {
     *  */
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    /* 某个用户关注的实体（用户、帖子等）
+    *  key: followee:userId:entityType
+    *  value: zset(entityId, now)
+    *
+    *  userId：关注者
+    *  entityType：关注的实体类型
+    *
+    *  entityId：关注的实体id
+    *  now：当前时间作为分数（按照关注的时间顺序列举）
+    * */
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    /* 某个实体拥有的粉丝
+    *  key: follower:entityType:entityId
+    *  value: zset(userId, now)
+    *
+    *  entityType：实体类型
+    *  entityId：实体id
+    *
+    *  userId：关注该实体的用户id
+    *  now：关注时间
+    *  */
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 }
